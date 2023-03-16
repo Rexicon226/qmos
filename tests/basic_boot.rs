2ca518082ba791b19099c8229cb21f14c70a6b10
David <87927264+Rexicon226@users.noreply.github.com>
2023-03-15 21:54:56 -0700

    correct test dir

---
+++ tests/basic_boot.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![reexport_test_harness_main = "test_main"]
+#![test_runner(qmos::test_runner)]
+
+use core::panic::PanicInfo;
+use qmos::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    qmos::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
